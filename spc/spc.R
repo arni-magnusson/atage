1130621b23808f4c93bfebfe7d41ed1c6a24a810
Arni Magnusson <thisisarni@gmail.com>
2025-09-28 09:23:18 +1100

    Start SPC analysis

---
+++ spc/spc.R
@@ -0,0 +1,32 @@
+fetch <- function(file, url, quiet=TRUE)
+{
+  if(file.exists(file))
+  {
+    if(!quiet)
+      message(file, " already exists")
+  }
+  else
+  {
+    download.file(url, file)
+  }
+}
+
+yft <- file.path("https://raw.githubusercontent.com/PacificCommunity",
+                 "/ofp-sam-yft-2023-diagnostic/refs/heads/main/TAF/output")
+
+yft.f <- file.path(yft, "f_season.csv")
+
+yft.n <- file.path(yft, "natage.csv")
+
+fetch("yft_2023_natage.csv", yft.n)
+fetch("yft_2023_fatage.csv", yft.f)
+
+n <- read.csv("yft_2023_natage.csv")
+f <- read.csv("yft_2023_fatage.csv")
+
+################################################################################
+
+f <- f[f$area == "all",]
+f$area <- NULL
+
+n <- aggregate(n~year+season+age, n, length)
